b9afa3c039fbb34be4bd56e0f24b60327cbb2ffa
= <anotidaekutsvaira@gmail.com>
2026-07-13 20:37:20 +0200

    Add payment and invoice mappers

--- src/main/java/mapper/InvoiceMapper.java
+++ src/main/java/mapper/InvoiceMapper.java
@@ -1,4 +1,13 @@
 package mapper;
 
+import dto.invoice.CreateInvoiceRequest;
+import entity.Invoice;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring" , uses = {InvoiceItemMapper.class, PaymentMapper.class })
 public interface InvoiceMapper {
+
+    @Mapping(target = "customer", source = "customerId")
+    Invoice toEntity(CreateInvoiceRequest request);
 }
