22431354509dc82c47334544765f486f40cb0443
= <anotidaekutsvaira@gmail.com>
2026-07-13 21:14:21 +0200

    Add InvoiceMapper

--- src/main/java/mapper/InvoiceMapper.java
+++ src/main/java/mapper/InvoiceMapper.java
@@ -1,13 +1,51 @@
 package mapper;
 
-import dto.invoice.CreateInvoiceRequest;
+import dto.invoice.*;
+import entity.Customer;
 import entity.Invoice;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring" , uses = {InvoiceItemMapper.class, PaymentMapper.class })
 public interface InvoiceMapper {
 
     @Mapping(target = "customer", source = "customerId")
     Invoice toEntity(CreateInvoiceRequest request);
+
+    @Mapping(target = "customerId" , source = "customer.id")
+    @Mapping(target = "customerName" , source = "customer.customerName")
+    InvoiceResponse toResponse (Invoice invoice);
+
+    @Mapping(target = "customerName", source = "customer.customerName")
+    InvoiceSummaryResponse toSummaryResponse(Invoice invoice);
+
+    @Mapping(target = "customerId", source = "customer.id")
+    @Mapping(target = "customerName", source = "customer.customerName")
+    @Mapping(target = "customerEmail", source = "customer.email")
+    @Mapping(target = "customerPhone", source = "customer.phone")
+    @Mapping(target = "customerAddress", source = "customer.address")
+    @Mapping(target = "customerCity", source = "customer.city")
+    @Mapping(target = "customerCountry", source = "customer.country")
+    @Mapping(target = "customerTaxNumber", source = "customer.taxNumber")
+    InvoiceDetailsResponse toDetailsResponse(Invoice invoice);
+
+    List<InvoiceResponse> toResponseList(List<Invoice> invoices);
+
+    List<InvoiceSummaryResponse> toSummaryResponseList(List<Invoice> invoices);
+
+    @Mapping(target = "customer" , source = "customerId")
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntityRequest(UpdateInvoiceRequest request, @MappingTarget Invoice invoice);
+
+    default Customer mapCustomer (Long customerId){
+        if(customerId == null){
+            return null;
+        }
+
+        Customer customer = new Customer();
+        customer.setId(customerId);
+        return customer;
+    }
+
 }
