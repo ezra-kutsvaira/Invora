2b4f41c37ef5b29ce72e5294542651c4e6dc24eb
= <anotidaekutsvaira@gmail.com>
2026-07-15 20:53:39 +0200

    Creating and getting customers implementation impl

--- src/main/java/repository/CustomerRepository.java
+++ src/main/java/repository/CustomerRepository.java
@@ -1,13 +1,14 @@
 package repository;
 
 import entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CustomerRepository extends CrudRepository<Customer,Long> {
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
 
     List<Customer> findByCustomerNameContainingIgnoreCase(String customerName);
 
