79aed1530ffe24fcb9d400fbecd2315042e18712
Ezra Anotida Kutsvaira <95180957+ezra-kutsvaira@users.noreply.github.com>
2026-08-10 15:00:15 +0200

    feat: isolate invoice items and payments by organization

--- src/main/java/com/ezra_anotida/invoice_maker/repository/InvoiceItemRepository.java
+++ src/main/java/com/ezra_anotida/invoice_maker/repository/InvoiceItemRepository.java
@@ -1,16 +1,11 @@
 package com.ezra_anotida.invoice_maker.repository;
 
-
-import com.ezra_anotida.invoice_maker.entity.Invoice;
 import com.ezra_anotida.invoice_maker.entity.InvoiceItem;
-import com.ezra_anotida.invoice_maker.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
+import java.util.Optional;
 
-public interface InvoiceItemRepository extends JpaRepository<InvoiceItem,Long> {
-
-    List<InvoiceItem> findByInvoice (Invoice invoice);
-
-    List<InvoiceItem> findByProduct (Product product);
+public interface InvoiceItemRepository extends JpaRepository<InvoiceItem, Long> {
+    Optional<InvoiceItem> findByIdAndInvoice_Organization_Id(Long itemId, Long organizationId);
+    List<InvoiceItem> findByInvoice_Organization_IdAndInvoice_Id(Long organizationId, Long invoiceId);
 }
