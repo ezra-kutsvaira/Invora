8593b2b35f39bd091950d0853cbfe570ffa8860a
Ezra Anotida Kutsvaira <95180957+ezra-kutsvaira@users.noreply.github.com>
2026-08-10 14:58:32 +0200

    feat: enforce organization scope for invoices

--- src/main/java/com/ezra_anotida/invoice_maker/service/InvoiceService.java
+++ src/main/java/com/ezra_anotida/invoice_maker/service/InvoiceService.java
@@ -1,31 +1,17 @@
 package com.ezra_anotida.invoice_maker.service;
 
-import com.ezra_anotida.invoice_maker.dto.invoice.CreateInvoiceRequest;
-import com.ezra_anotida.invoice_maker.dto.invoice.InvoiceResponse;
-import com.ezra_anotida.invoice_maker.dto.invoice.InvoiceSummaryResponse;
-import com.ezra_anotida.invoice_maker.dto.invoice.UpdateInvoiceRequest;
-
+import com.ezra_anotida.invoice_maker.dto.invoice.*;
 import java.util.List;
 
 public interface InvoiceService {
-
-    InvoiceResponse createInvoice(CreateInvoiceRequest createInvoiceRequest);
-
-    InvoiceResponse getInvoiceById(Long invoiceId);
-
-    InvoiceResponse getInvoiceByInvoiceNumber(String invoiceNumber);
-
-    List<InvoiceResponse> getAllInvoices();
-
-    List<InvoiceSummaryResponse> getInvoiceSummaries();
-
-    List<InvoiceResponse> getInvoicesByCustomer(Long customerId);
-
-    InvoiceResponse updateInvoice(Long invoiceId, UpdateInvoiceRequest request);
-
-    InvoiceResponse issueInvoice(Long invoiceId);
-
-    InvoiceResponse cancelInvoice(Long invoiceId);
-
-    void deleteInvoice(Long invoiceId);
+    InvoiceResponse createInvoice(Long organizationId, CreateInvoiceRequest request);
+    InvoiceResponse getInvoiceById(Long organizationId, Long invoiceId);
+    InvoiceResponse getInvoiceByInvoiceNumber(Long organizationId, String invoiceNumber);
+    List<InvoiceResponse> getAllInvoices(Long organizationId);
+    List<InvoiceSummaryResponse> getInvoiceSummaries(Long organizationId);
+    List<InvoiceResponse> getInvoicesByCustomer(Long organizationId, Long customerId);
+    InvoiceResponse updateInvoice(Long organizationId, Long invoiceId, UpdateInvoiceRequest request);
+    InvoiceResponse issueInvoice(Long organizationId, Long invoiceId);
+    InvoiceResponse cancelInvoice(Long organizationId, Long invoiceId);
+    void deleteInvoice(Long organizationId, Long invoiceId);
 }
