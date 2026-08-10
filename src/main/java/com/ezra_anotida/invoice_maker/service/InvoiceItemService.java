b45fef0d7bb69c26d5408f5ec6e05b728245c204
Ezra Anotida Kutsvaira <95180957+ezra-kutsvaira@users.noreply.github.com>
2026-08-10 15:00:10 +0200

    feat: isolate invoice items and payments by organization

--- src/main/java/com/ezra_anotida/invoice_maker/service/InvoiceItemService.java
+++ src/main/java/com/ezra_anotida/invoice_maker/service/InvoiceItemService.java
@@ -1,20 +1,12 @@
 package com.ezra_anotida.invoice_maker.service;
 
-import com.ezra_anotida.invoice_maker.dto.invoiceitem.CreateInvoiceItemRequest;
-import com.ezra_anotida.invoice_maker.dto.invoiceitem.InvoiceItemResponse;
-import com.ezra_anotida.invoice_maker.dto.invoiceitem.UpdateInvoiceItemRequest;
-
+import com.ezra_anotida.invoice_maker.dto.invoiceitem.*;
 import java.util.List;
 
 public interface InvoiceItemService {
-
-    InvoiceItemResponse addItemToInvoice(Long invoiceId, CreateInvoiceItemRequest request);
-
-    InvoiceItemResponse getInvoiceItemById(Long invoiceItemId);
-
-    List<InvoiceItemResponse> getItemsByInvoice(Long invoiceId);
-
-    InvoiceItemResponse updateInvoiceItem(Long invoiceItemId, UpdateInvoiceItemRequest request);
-
-    void removeInvoiceItem(Long invoiceItemId);
+    InvoiceItemResponse addItemToInvoice(Long organizationId, Long invoiceId, CreateInvoiceItemRequest request);
+    InvoiceItemResponse getInvoiceItemById(Long organizationId, Long invoiceItemId);
+    List<InvoiceItemResponse> getItemsByInvoice(Long organizationId, Long invoiceId);
+    InvoiceItemResponse updateInvoiceItem(Long organizationId, Long invoiceItemId, UpdateInvoiceItemRequest request);
+    void removeInvoiceItem(Long organizationId, Long invoiceItemId);
 }
